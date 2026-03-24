eda1a9f319b3e54ab068a210a5b046a0cce80fa6
MackonAjax <ajaxmackon@gmail.com>
2026-03-24 21:37:51 +0300

    Mandre Derrick Ajax here. This will do

---
+++ ex2_fuel.c
@@ -0,0 +1,19 @@
+// MANDRE DERRICK AJAX
+// 25/U/BIO/01391/PD
+
+
+#include <stdio.h>
+
+int main(){
+    float distance_travelled, fuel_used, fuel_efficiency;
+
+    printf("Enter distance(km): ");
+    scanf("%f", &distance_travelled);
+
+    printf("Enter fuel used (litres): ");
+    scanf("%f", &fuel_used);
+
+    fuel_efficiency = distance_travelled / fuel_used;
+    printf("\nFuel efficiency: %.2f km/l", fuel_efficiency);
+    return 0;
+}
